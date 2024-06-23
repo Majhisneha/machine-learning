e967e446bf712a825a9b1ab4da7291ace7f2d3b2
Enis Kerem Çakmak <eniskeremcakmak@gmail.com>
2024-06-23 14:48:43 +0200

    Create test_enis.py

---
+++ pybrain/tests/unittests/test_enis.py
@@ -0,0 +1,10 @@
+from pybrain.tools.plotting.multiline import MultilinePlotter
+
+plotter = MultilinePlotter(maxLines=3)
+plotter.addData(0, 1, 2)
+plotter.addData(1, [3, 4], [5, 6])
+plotter.currentID = 0
+plotter.setLineStyle(color="red", linestyle="--")
+plotter.setLineStyle(id=[0, 1], color="blue", linestyle="-.")
+plotter.setLineStyle(id=1, color="green", linestyle=":")
+plotter.setLineStyle(id=-1, color="black", linestyle="-")
